8062bcabce92755b347356ab6362f1b5bcea3584
lotus496 <yuremo0628@gmail.com>
2026-08-31 13:44:21 +0000

    お題3-2まで終了 一覧から詳細ページを見れるようになった

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -1,9 +1,16 @@
 class TutorialsController < ApplicationController
   def index
     @profiles = TutorialProfile.published.sort_by(&:name)
+    if params[:category].present?
+      @profiles = @profiles.select { |profile| profile.category == params[:category] }
+    end
   end
 
   def debug
     @profile = TutorialProfile.find(1)
+  end
+
+  def show
+    @profile = TutorialProfile.find(params[:id])
   end
 end
