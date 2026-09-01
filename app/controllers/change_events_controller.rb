a1951289c6ce28ec04eb6403a87b78b6f6ff4ba5
lotus496 <yuremo0628@gmail.com>
2026-09-01 05:30:54 +0000

    神回:debugからパクってきたChangeEvetsControllerを使って変更一覧を表示できた

---
+++ app/controllers/change_events_controller.rb
@@ -0,0 +1,41 @@
+class ChangeEventsController < ApplicationController
+  before_action :ensure_debug_enabled
+
+  def index
+    @change_events = ChangeEvent.source_debug.recent.limit(20)
+  end
+
+  def create
+    case params.require(:kind)
+    when "pending"
+      ChangeEvent.create_debug!(review_status: :pending)
+    when "reviewed"
+      ChangeEvent.create_debug!(review_status: :reviewed)
+    when "examples"
+      ChangeEvent.create_debug_examples!
+    else
+      raise ActionController::BadRequest,
+        "未対応のデバッグ操作です"
+    end
+
+    redirect_to(
+      debug_change_events_path,
+      notice: "表示確認用の変更記録を追加しました。"
+    )
+  end
+
+  def destroy
+    ChangeEvent.remove_debug!(id: params[:id])
+
+    redirect_to(
+      debug_change_events_path,
+      notice: "表示確認用の変更記録を削除しました。"
+    )
+  end
+
+  private
+
+  def ensure_debug_enabled
+    head :not_found unless ChangeEvent.debug_enabled?
+  end
+end
